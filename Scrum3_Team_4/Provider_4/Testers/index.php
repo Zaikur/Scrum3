b5988dd448c3e1ec3b4eb88f44555184c2255222
Quinton Nelson <124801789+Zaikur@users.noreply.github.com>
2024-03-23 11:38:04 -0500

    Added and updated some styles

--- Scrum3_Team_4/Provider_4/Testers/index.php
+++ Scrum3_Team_4/Provider_4/Testers/index.php
@@ -3,12 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../styles/styles.css">
     <title>Tester</title>
    
 </head>
 <body>
     <h1>Database Tester</h1>
-    <ul>
+    <ul class="testerList">
         <li><a href="test_api.html">Test API (JSON Format)</a></li>
         <li><a href="display_tables.php">Display Tables in HTML</a></li>
     </ul>
